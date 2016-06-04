b8aaafe05ddc106d31e618df33805b909f0a0a10
mbolanosc <mrybc96@gmail.com>
2016-06-03 20:57:26 -0600

    contenido en controlador

--- controllers.js
+++ controllers.js
@@ -1,5 +1,13 @@
 /**
- * angular.module retorna un modulo al que se le da .controller 
+ * Angular.module retorna un modulo al que se le da .controller.
+ * No más $scope como dependencia.
  */
 angular.module('todoapp'[])
-	.controller();
+	.controller('todoController', function(){
+		var todo = this;
+			todo.list =[
+				(text: 'Aprender angularJS.', done: true),
+				(text: 'Dar de comer a mis perros.', done: false),
+				(text: 'No usar más $scope.', done: false)
+		];
+	});
