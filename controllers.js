ca3405aadb7897449f242c5f657525a353ccc3ee
mbolanosc <mrybc96@gmail.com>
2016-06-08 19:15:06 -0600

    mas funcionalidades

--- controllers.js
+++ controllers.js
@@ -2,12 +2,16 @@
  * Angular.module retorna un modulo al que se le da .controller.
  * No más $scope como dependencia.
  */
-angular.module('todoapp'[])
+angular.module('todoapp',[])
 	.controller('todoController', function(){
 		var todo = this;
 			todo.list =[
-				(text: 'Aprender angularJS.', done: true),
-				(text: 'Dar de comer a mis perros.', done: false),
-				(text: 'No usar más $scope.', done: false)
+				{text: 'Aprender angularJS.', done: true},
+				{text: 'Dar de comer a mis perros.', done: false},
+				{text: 'No usar más $scope.', done: false}
 		];
+
+		todo.add = function(){
+			todo.todos.push(());
+		}
 	});
